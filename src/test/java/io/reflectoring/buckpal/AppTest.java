dc4acd29cd07006239360d1e2cd2aae74bafd56b
Fabio Santos Almeida <livre.programacao@gmail.com>
2021-03-11 17:58:18 -0300

    First commit to buckpal application.

---
+++ src/test/java/io/reflectoring/buckpal/AppTest.java
@@ -0,0 +1,21 @@
+package io.reflectoring.buckpal;
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+}
